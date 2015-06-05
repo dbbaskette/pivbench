4de0df3f57bfe9bd7f0a9b567644bd1e72a6c8aa
dbbaskette <dbbaskette@gmail.com>
2015-06-05 18:53:45 -0400

    Adding support for single table gen

--- utils/Hadoop.py
+++ utils/Hadoop.py
@@ -54,7 +54,7 @@ def appStatus(resourceManager):
     appsJSON = json.loads(response.text)
     for app in appsJSON["apps"]:
         print app['id']
-
+    exit()
 # cd tpcds-gen; hadoop jar target/*.jar -d ${DIR}/${SCALE}/ -s ${SCALE})
 # url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/PHD-3.0"
 # requests.put(url, auth=auth, headers=headers, data=payload)
