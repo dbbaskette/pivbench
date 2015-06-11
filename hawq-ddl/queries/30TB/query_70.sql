ab6bb9e18d22415a27004a1f5eb0d789e9c08c1a
dbbaskette <dbbaskette@gmail.com>
2015-06-11 18:58:26 -0400

    Add all queries

--- hawq-ddl/queries/30TB/query_70.sql
+++ hawq-ddl/queries/30TB/query_70.sql
@@ -14,7 +14,7 @@ select  * from (
    ,date_dim       d1
    ,store
  where
-    d1.d_month_seq between 1212 and 1212+11
+    d1.d_month_seq between 1193 and 1193+11
  and d1.d_date_sk = ss_sold_date_sk
  and s_store_sk  = ss_store_sk
  and s_state in
@@ -22,7 +22,7 @@ select  * from (
                from  (select s_state as s_state,
  			    rank() over ( partition by s_state order by sum(ss_net_profit) desc) as ranking
                       from   store_sales, store, date_dim
-                      where  d_month_seq between 1212 and 1212+11
+                      where  d_month_seq between 1193 and 1193+11
  			    and d_date_sk = ss_sold_date_sk
  			    and s_store_sk  = ss_store_sk
                       group by s_state
