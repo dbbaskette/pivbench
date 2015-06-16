fa40ca8b0d28fcc78e0f90586eca38432281e3f8
dbbaskette <dbbaskette@gmail.com>
2015-06-15 22:35:26 -0400

    parquet blocksize fix

--- hawq-ddl/hawq-part/inventory.sql
+++ hawq-ddl/hawq-part/inventory.sql
@@ -6,7 +6,7 @@ create table inventory
     inv_warehouse_sk          integer               not null,
     inv_quantity_on_hand      integer
 )
-with (appendonly=true, ORIENTATION=$ORIENTATION,ROWGROUPSIZE=1073741823)
+with (appendonly=true, ORIENTATION=$ORIENTATION,ROWGROUPSIZE=1073741823,COMPRESSTYPE=snappy)
 distributed by (inv_date_sk, inv_item_sk, inv_warehouse_sk)
 partition by range(inv_date_sk)
 (start(2450815) INCLUSIVE end(2453005) INCLUSIVE every ($PARTS),
--- hawq-ddl/hawq-part/web_sales.sql
+++ hawq-ddl/hawq-part/web_sales.sql
@@ -36,7 +36,7 @@ create table web_sales
     ws_net_paid_inc_ship_tax  decimal(7,2)                  ,
     ws_net_profit             decimal(7,2)
 )
-with (appendonly=true, ORIENTATION=$ORIENTATION,ROWGROUPSIZE=1073741823)
+with (appendonly=true, ORIENTATION=$ORIENTATION,ROWGROUPSIZE=1073741823,COMPRESSTYPE=snappy)
 distributed by (ws_item_sk, ws_order_number)
 partition by range(ws_sold_date_sk)
 (start(2450815) INCLUSIVE end(2453005) INCLUSIVE every ($PARTS),
